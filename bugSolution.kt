5b7817277a4b296038def8212a700a07cc40d515
BugHunter-X <askzenai@gmail.com>
2024-12-24 17:56:05 +0100

    Add bugSolution.kt file

---
+++ bugSolution.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val list = mutableListOf<Int>(1, 2, 3, 4, 5)
+    val iterator = list.iterator()
+    while (iterator.hasNext()) {
+        if (iterator.next() % 2 == 0) {
+            iterator.remove()
+        }
+    }
+    println(list)
+}
